4ff7a763116b7fd5e39a4f6dc25dc094746c2d06
Abu Sayeed <sayeed@Abus-Mac-mini.local>
2025-05-19 17:25:17 -0400

    done working with category loading, also pet section design

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,25 @@
-console.log('hello')
+const loadCategories = async() => {
+    try{
+        const response = await fetch(`https://openapi.programming-hero.com/api/peddy/categories`);
+        const data = await response.json();
+        showCategories(data.categories);
+    } catch(error){
+        console.log(`Erro:${error}`);
+    }
+}
+
+const showCategories = (categories) => {
+    const categoryContainer = document.getElementById('category-container');
+    categories.forEach(categoryItem => {
+        const categoryButton = document.createElement('button');
+        categoryButton.className = "btn border-2 p-6 rounded-lg font-bold";
+        categoryButton.id = categoryItem.id;
+        categoryButton.innerHTML = 
+        `
+        <img class="w-8" src="${categoryItem.category_icon}" alt=""> ${categoryItem.category}
+        `;
+        categoryContainer.appendChild(categoryButton);
+    });
+}
+
+loadCategories();
