1377020ec59e73ab93719aae9bc2eb3a68e18bcd
Abu Sayeed <sayeed@Abus-Mac-mini.local>
2025-05-21 21:06:14 -0400

    added show details, added liked pet, still working

--- scripts/app.js
+++ scripts/app.js
@@ -66,6 +66,7 @@ const loadPetsByCategory = async (categoryId, categoryName) => {
 
 const showPets = (pets) => {
   const petsContainer = document.getElementById("pet-container");
+  petsContainer.innerHTML = "";
 
   pets.forEach((petItem) => {
     const petsCard = document.createElement("div");
@@ -100,17 +101,110 @@ const showPets = (pets) => {
                 </div>
                 <hr class="border-1 border-gray-200 my-2" />
                 <div class="flex justify-between text-[#0E7A81] font-bold">
-                <button class="btn border-2 rounded-lg px-3 py-2">
+                <button id="petid-${
+                  petItem.petId
+                }" class="btn border-2 rounded-lg px-3 py-2">
                 <i class="fa-solid fa-thumbs-up"></i></button
                 ><button class="btn border-2 rounded-lg px-3 py-2">
                 Adopt</button
-                ><button class="btn border-2 rounded-lg px-3 py-2">
+                ><button id="details-btn-${
+                  petItem.petId
+                }" onclick="detailsModal.showModal()" class="btn border-2 rounded-lg px-3 py-2">
                 Details
                 </button>
             </div>
         </div>
     `;
+
     petsContainer.appendChild(petsCard);
+    const detailsBtn = document.getElementById(`details-btn-${petItem.petId}`);
+    detailsBtn.addEventListener("click", (e) => {
+      showDetails(petItem.petId);
+    });
+    const likeBtn = document.getElementById(`petid-${petItem.petId}`);
+    likeBtn.addEventListener("click", () => {
+      likedPetsList(petItem, pets);
+    });
+  });
+};
+const showDetails = async (petId) => {
+  try {
+    const modalContainer = document.getElementById("modal-data");
+    modalContainer.innerHTML = "";
+    toggleDetailsSpinner(true);
+    const response = await fetch(
+      `https://openapi.programming-hero.com/api/peddy/pet/${petId}`
+    );
+    const data = await response.json();
+    const petData = data.petData;
+    const modalCard = document.createElement("div");
+    // modalCard.classList.add("modal-box");
+    modalCard.innerHTML = `
+        <div>
+        <img
+            class="w-full rounded-lg mb-3"
+            src="${petData.image}"
+            alt="${petData.name}"
+        />
+        <h2 class="font-bold text-lg mb-3">${petData.pet_name}</h2>
+        <div class="grid grid-cols-2 w-5/6 gap-4 text-gray-400 mb-3">
+            <p><i class="fa-solid fa-puzzle-piece"></i> breed: ${
+              petData.breed
+            }</p>
+            <p><i class="fa-solid fa-calendar-days"></i> Birth: ${
+              petData.date_of_birth
+                ? getBirthYear(petData?.date_of_birth)
+                : "not found"
+            }</p>
+            <p><i class="fa-solid fa-mars-and-venus"></i> Gender: ${
+              petData.gender
+            }</p>
+            <p><i class="fa-solid fa-dollar-sign"></i> Price: ${
+              petData.price ? "$" + petData.price : "contact the seller"
+            }</p>
+            <p><i class="fa-solid fa-syringe"></i> Vaccine: ${
+              petData.vaccinated_status
+            } </p>
+        </div>
+        <hr class="border-1 border-gray-200 my-3" />
+        <h3 class="font-bold mb-3">Details Information</h3>
+        <p class="text-gray-400 mb-3">
+        ${petData.pet_details}
+        </p>
+        </div>
+        <!-- modal close btn  -->
+        <form method="dialog">
+        <button
+            class="btn w-full text-center border-1 rounded-lg bg-[#0e79810d] text-[#0E7A81]"
+        >
+            Cancel
+        </button>
+        </form>
+    `;
+    modalContainer.appendChild(modalCard);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  } finally {
+    toggleDetailsSpinner(false);
+  }
+};
+const likedPets = [];
+const likedPetsList = (liked, loadedPets) => {
+  console.log(liked, loadedPets);
+  const matching = likedPets.some((pet) => pet.petId === liked.petId);
+  if (!matching) {
+    likedPets.push(liked);
+  } else {
+    console.log("duplicate");
+  }
+  const likedPetsContainer = document.getElementById("liked-pets");
+  likedPetsContainer.innerHTML = "";
+  likedPets.forEach((likedItem) => {
+    const likedPetCard = document.createElement("img");
+    likedPetCard.class = "rounded-lg w-full";
+    likedPetCard.setAttribute("src", likedItem.image);
+
+    likedPetsContainer.appendChild(likedPetCard);
   });
 };
 const getBirthYear = (date) => {
@@ -126,9 +220,17 @@ const toggleCategorySpinner = (isActive) => {
     categorySpinner.classList.add("hidden");
   }
 };
+const toggleDetailsSpinner = (isActive) => {
+  const detailsSpinner = document.getElementById("details-spinner");
+  if (isActive == true) {
+    detailsSpinner.classList.remove("hidden");
+  } else if (isActive == false) {
+    detailsSpinner.classList.add("hidden");
+  }
+};
 const togglePetSpinner = (isActive) => {
   const petSpinner = document.getElementById("pet-spinner");
-  const likedPet = document.getElementById("liked-pet");
+  const likedPet = document.getElementById("liked-pets");
   if (isActive == true) {
     petSpinner.classList.remove("hidden");
     likedPet.classList.add("hidden");
@@ -165,3 +267,18 @@ loadPets();
 //     "vaccinated_status": "Fully",
 //     "pet_name": "Mia"
 //   },
+
+/* <dialog id="detailsModal" class="modal modal-bottom sm:modal-middle">
+            <div class="modal-box">
+              <h3 class="text-lg font-bold">Hello!</h3>
+              <p class="py-4">
+                Press ESC key or click the button below to close
+              </p>
+              <div class="modal-action">
+                <form method="dialog">
+                  <!-- if there is a button in form, it will close the modal -->
+                  <button class="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog> */
